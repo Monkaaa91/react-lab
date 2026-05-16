8d2f17649ef78311426bd6700672ab5ec9c19beb
Monika <mondrzejewska.m@gmail.com>
2026-05-16 10:23:32 +0200

    second commit warunki logiczne

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  let email = 'mmondrzej@student.agh.edu.pl';
-  function handleChange(event) {
-    console.log(event.target.value);
-  }
-  return (
-    <div className="App">
-     <h1>System do zapisów na zajęcia</h1>
-      <h2>Twój e-mail to {email}</h2>
-      <input type="text" onChange={handleChange}/>
-    </div>
-  )
+    const [email, setEmail] = useState('');
+
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+
+    return (
+        <div className="App">
+            <h1>System do zapisów na zajęcia</h1>
+
+            <h2>Twój e-mail to {email}</h2>
+
+            <input
+                type="text"
+                value={email}
+                onChange={handleChange}/>
+
+            {email.length > 0 && email.length < 20 && (
+                <p className="error">Email jest za krótki.</p>)}
+
+            {email.length >= 30 && (
+                <p className="error" >Email jest za długi.</p>)}
+        </div>
+    );
 }
 
 export default App;
+
+
